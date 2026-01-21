f6bbba8e8806310e2b11c2644fd61e57c170c2a5
jacquelinepb0 <jac.pb14@gmail.com>
2026-01-21 11:51:55 -0300

    intro js 21/01

---
+++ metodos.js
@@ -0,0 +1,28 @@
+// Qual o resultado da seguinte expressão?var total = 10 + 5 * 2 / 2 + 20;
+
+var resultado = (10 + 5 * 2) / (2 + 20);
+console.log(resultado)
+
+
+// Crie duas expressões que retornem NaN
+
+const number = Number("10%")
+let numero2 = parseInt("x")
+console.log(number)
+console.log(numero2)
+
+const temperatura = [-40, -50, -50, -30]
+
+const media = ((-40) + (-50) + (-50) + (-30)) / 4
+console.log(media)
+
+// Somar a string '200' com o número 50 e retornar 250
+
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+
+// Como dividir o peso por 2? 
+var numero = '60';
+var unidade = 'kg';
+var peso = numero + unidade;
