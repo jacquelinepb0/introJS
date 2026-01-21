848879c73e22247eeeed1a1b685f846c478e0cae
jacquelinepb0 <jac.pb14@gmail.com>
2026-01-21 11:04:23 -0300

    intro js

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.log("hello world");
+
+
+//tipos de dados
+
+//String
+"Jacqueline"
+`Jacqueline`
+'Jacqueline'
+
+//Number
+0
+10.5
+
+//Boolean
+True / false
+
+
+//Null
+null
+
+//Indefined
+undefined
+
+//-------------
+
+//Variáveis
+
