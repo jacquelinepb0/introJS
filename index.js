ce46730c6fb0cd4ebe3b657832bb5606a42d92df
jacquelinepb0 <jac.pb14@gmail.com>
2026-01-28 08:19:46 -0300

    introJS

--- index.js
+++ index.js
@@ -26,3 +26,42 @@ undefined
 
 //Variáveis
 
+let fruta = "Banana";
+
+fruta = "Maçã";
+
+//Estrutura de Função
+function nomeDaFuncao() {
+  let fruta = "Uva";
+  console.log(fruta);
+}
+
+let turma;
+
+//Estrutura de Array
+turma = ["Iasmin", "Matheus", "Mário"];
+
+//Estrutura de Objeto
+let pessoa = {
+  nome: "Nayara",
+  idade: 30,
+  profissao: "Desenvolvedora Fullstack",
+};
+
+//Array de Objeto
+let produtos = [
+  {
+    nome: "Tênis",
+    preco: 199.9,
+    categoria: "Calçados",
+    cor: ["branco", "azul"],
+    tamanho: [36, 37, 38, 39, 40, 41],
+  },
+  {
+    nome: "Chinelo",
+    preco: 50.0,
+    categoria: "Calçados",
+    cor: ["branco", "branco-encardido"],
+    tamanho: [36, 37, 38, 39],
+  },
+];
